b90e685b33582d9021b92bdafe0338e64f85a797
vaishnavisharma810 <vaishnavisharma137@gmail.com>
2021-08-08 12:11:01 +0530

    Week 3 Added

---
+++ Week 3/Ques1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int a[], int n)
+{
+    int comparisons = 0, shifts = 0;
+    for(int i = 1; i < n; i++)
+    {
+        int key = a[i];
+        int j = i - 1;
+        while(j >= 0 && key < a[j])
+        {
+            a[j + 1] = a[j];
+            j = j - 1;
+            comparisons++;
+            shifts++;
+        }
+        a[j + 1] = key;
+        shifts++;
+    }
+    for(int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << "\nComparisons = " << comparisons << endl;
+    cout << "Shifts = " << shifts << endl;
+}
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  {
+      int n;
+      cin >> n;
+      int *a = new int[n];
+      for(int i = 0; i < n; i++)
+        cin >> a[i];
+      insertionSort(a, n);
+  }
+}
